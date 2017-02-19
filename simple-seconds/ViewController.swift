b8214a0b0788016ea07d710e424bd4334f15373e
Marvin Messenzehl <marvin.messenzehl@gmail.com>
2017-02-19 19:47:43 +0100

    Added the basic interface and outlets

--- simple-seconds/ViewController.swift
+++ simple-seconds/ViewController.swift
@@ -10,16 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    // MARK: - Outlets from storyboard
+    
+    @IBOutlet weak var secondsLabel: UILabel!
+    @IBOutlet weak var secondsSlider: UISlider!
+    
+    @IBAction func sliderChanged(_ sender: Any) {
+    }
+    
+    @IBAction func activateTapped(_ sender: Any) {
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 

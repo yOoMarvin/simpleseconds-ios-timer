2cc22644fb316257cc41b09003497cdf652a6e20
Marvin Messenzehl <marvin.messenzehl@gmail.com>
2017-02-22 17:17:11 +0100

    Displayed the notification! (only when app is inactive

--- simple-seconds/ViewController.swift
+++ simple-seconds/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 import UserNotifications
 
+let notificationIdentifier = "com.marvinmessenzehl.simple-seconds"
+
 class ViewController: UIViewController {
 
     
@@ -26,12 +28,32 @@ class ViewController: UIViewController {
         let notification = SSNotification()
         let content = notification.getContent()
         let trigger = notification.getTimeTrigger(forSeconds: getSeconds())
+        
+        let request = UNNotificationRequest(identifier: notificationIdentifier, content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request) {
+            (error) in
+            
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            
+            self.present(self.getDialog(), animated: true, completion: nil)
+        }
     }
     
     
     
     private func getSeconds () -> Int {
         return Int(secondsSlider.value)
+    }
+    
+    private func getDialog () -> UIAlertController {
+        let alert = UIAlertController(title: "Timer started", message: "Your tea countdown started", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        
+        return alert
     }
     
     
